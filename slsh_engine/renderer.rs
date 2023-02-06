5aca0dadb19ff6f3e3dc2076360e00f51747ec2c
Ruslan Akbashev <ruslashev@gmail.com>
2023-02-07 00:29:50 +0400

    engine/renderer: create instance and pick physical device

--- slsh_engine/renderer.rs
+++ slsh_engine/renderer.rs
@@ -1,35 +1,236 @@
-use std::ffi::{c_char, CString};
+use std::default::Default;
+use std::ffi::{c_char, CStr, CString};
+use std::fmt::Display;
+use std::str::FromStr;
 
+use ash::extensions::khr::Surface;
 use ash::vk;
 
 use crate::window::Window;
 
+const REQ_VALIDATION_LAYERS: &[&str] = &["VK_LAYER_KHRONOS_validation"];
+const API_VER_MAJOR: u32 = 1;
+const API_VER_MINOR: u32 = 0;
+const API_VER_PATCH: u32 = 0;
+
+trait CheckVkError<T> {
+    fn check_err(self, action: &'static str) -> T;
+}
+
 pub struct Renderer {
+    entry: ash::Entry,
     instance: ash::Instance,
 }
 
+#[derive(Clone)]
+struct PhysDeviceInfo {
+    phys_device: vk::PhysicalDevice,
+    properties: vk::PhysicalDeviceProperties,
+    queue_families: Vec<vk::QueueFamilyProperties>,
+    extensions: Vec<vk::ExtensionProperties>,
+    queue_family_idx: i32,
+}
+
+#[derive(Clone, Debug, Copy)]
+struct Vertex {
+    pos: [f32; 4],
+    color: [f32; 4],
+}
+
 impl Renderer {
-    pub fn new(window: &Window) -> Self {
+    pub unsafe fn new(app_name: &'static str, window: &Window) -> Self {
+        let req_exts_owned = window.get_required_extensions();
+        let req_exts_cstrs = convert_to_c_strs(&req_exts_owned);
+
         let entry = ash::Entry::linked();
-        let req_exts = window.get_required_extensions();
-        let req_exts_cstrs = convert_to_c_strs(&req_exts);
-        let req_exts_cptrs = convert_to_c_ptrs(&req_exts_cstrs);
-        let info = vk::InstanceCreateInfo::builder().enabled_extension_names(&req_exts_cptrs);
+        let instance = create_instance(app_name, &req_exts_cstrs, &entry);
+        let surface = window.create_surface(&instance);
+        let phys_device = pick_phys_device(&entry, &instance, surface, &req_exts_cstrs);
 
-        let instance =
-            unsafe { entry.create_instance(&info, None).expect("Failed to create instance") };
-
-        Self { instance }
+        Self { instance, entry }
     }
+}
+
+impl Drop for Renderer {
+    fn drop(&mut self) {
+        unsafe {}
+    }
+}
+
+impl<T> CheckVkError<T> for Option<T> {
+    fn check_err(self, action: &'static str) -> T {
+        match self {
+            Some(t) => t,
+            None => panic!("Failed to {}", action),
+        }
+    }
+}
+
+impl<T, E: Display> CheckVkError<T> for Result<T, E> {
+    fn check_err(self, action: &'static str) -> T {
+        match self {
+            Ok(t) => t,
+            Err(e) => panic!("Failed to {}: err = {}", action, e),
+        }
+    }
+}
+
+fn create_instance(
+    app_name: &'static str,
+    req_exts: &[CString],
+    entry: &ash::Entry,
+) -> ash::Instance {
+    let app_cstring = CString::new(app_name).check_err("convert app_name to CString");
+    let app_cstr = app_cstring.as_c_str();
+
+    let engine_name = CStr::from_bytes_with_nul(b"slsh_engine\0").unwrap();
+
+    let eng_ver_major = u32::from_str(env!("CARGO_PKG_VERSION_MAJOR")).unwrap();
+    let eng_ver_minor = u32::from_str(env!("CARGO_PKG_VERSION_MINOR")).unwrap();
+    let eng_ver_patch = u32::from_str(env!("CARGO_PKG_VERSION_PATCH")).unwrap();
+    let engine_version = vk::make_api_version(0, eng_ver_major, eng_ver_minor, eng_ver_patch);
+
+    let api_version = vk::make_api_version(0, API_VER_MAJOR, API_VER_MINOR, API_VER_PATCH);
+
+    let app_info = vk::ApplicationInfo::builder()
+        .application_name(app_cstr)
+        .application_version(0)
+        .engine_name(engine_name)
+        .engine_version(engine_version)
+        .api_version(api_version);
+
+    let req_layers_owned = REQ_VALIDATION_LAYERS.iter().map(|l| l.to_string()).collect::<Vec<_>>();
+    let req_layers_cstrs = convert_to_c_strs(&req_layers_owned);
+    let req_layers_cptrs = convert_to_c_ptrs(&req_layers_cstrs);
+
+    let mut req_exts_cptrs = convert_to_c_ptrs(&req_exts);
+
+    // #[cfg(any(target_os = "macos", target_os = "ios"))]
+    {
+        req_exts_cptrs.push(vk::KhrPortabilityEnumerationFn::name().as_ptr());
+        req_exts_cptrs.push(vk::KhrGetPhysicalDeviceProperties2Fn::name().as_ptr());
+    }
+
+    let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::default()
+    };
+
+    let create_info = vk::InstanceCreateInfo::builder()
+        .application_info(&app_info)
+        .enabled_layer_names(&req_layers_cptrs)
+        .enabled_extension_names(&req_exts_cptrs)
+        .flags(create_flags);
+
+    unsafe { entry.create_instance(&create_info, None) }.check_err("create instance")
 }
 
 fn convert_to_c_strs(strings: &[String]) -> Vec<CString> {
     strings
         .iter()
-        .map(|string| CString::new(string.clone()).expect("String contains null byte"))
+        .map(|string| CString::new(string.clone()).check_err("convert to CString"))
         .collect()
 }
 
 fn convert_to_c_ptrs(cstrings: &[CString]) -> Vec<*const c_char> {
     cstrings.iter().map(|cstring| cstring.as_ptr()).collect()
+}
+
+unsafe fn pick_phys_device(
+    entry: &ash::Entry,
+    instance: &ash::Instance,
+    surface: vk::SurfaceKHR,
+    req_exts: &[CString],
+) -> PhysDeviceInfo {
+    let phys_devices = instance.enumerate_physical_devices().check_err("get physical devices");
+    let surface_loader = Surface::new(&entry, &instance);
+    let mut phys_device_infos =
+        gather_phys_device_infos(instance, surface, surface_loader, &phys_devices, req_exts);
+
+    assert!(!phys_device_infos.is_empty(), "No suitable devices found");
+
+    phys_device_infos.sort_by_key(|d| device_type_to_priority(d.properties.device_type));
+
+    phys_device_infos[0].clone()
+}
+
+unsafe fn gather_phys_device_infos(
+    instance: &ash::Instance,
+    surface: vk::SurfaceKHR,
+    surface_loader: Surface,
+    phys_devices: &[vk::PhysicalDevice],
+    req_exts: &[CString],
+) -> Vec<PhysDeviceInfo> {
+    let mut phys_device_infos = Vec::with_capacity(phys_devices.len());
+
+    for device_ref in phys_devices {
+        let phys_device = *device_ref;
+
+        let properties = instance.get_physical_device_properties(phys_device);
+        let queue_families = instance.get_physical_device_queue_family_properties(phys_device);
+        let extensions = instance
+            .enumerate_device_extension_properties(phys_device)
+            .check_err("enumerate device extensions");
+
+        let mut queue_family = None;
+
+        for (i, qf) in queue_families.iter().enumerate() {
+            let graphics_support = qf.queue_flags.contains(vk::QueueFlags::GRAPHICS);
+            let surface_support = surface_loader
+                .get_physical_device_surface_support(phys_device, i as u32, surface)
+                .check_err("get surface support");
+
+            if graphics_support && surface_support {
+                let idx = i.try_into().unwrap();
+                queue_family = Some(idx);
+                break;
+            }
+        }
+
+        if !supports_required_extensions(&extensions, req_exts) {
+            continue;
+        }
+
+        if let Some(queue_family_idx) = queue_family {
+            let info = PhysDeviceInfo {
+                phys_device,
+                properties,
+                queue_families,
+                extensions,
+                queue_family_idx,
+            };
+
+            phys_device_infos.push(info);
+        }
+    }
+
+    phys_device_infos
+}
+
+fn supports_required_extensions(exts: &[vk::ExtensionProperties], req_exts: &[CString]) -> bool {
+    let mut support_found = Vec::with_capacity(req_exts.len());
+    support_found.resize(req_exts.len(), false);
+
+    for (i, req_ext) in req_exts.into_iter().enumerate() {
+        for ext in exts {
+            let name = unsafe { CStr::from_ptr(ext.extension_name.as_ptr()) };
+
+            if name == req_ext.as_c_str() {
+                support_found[i] = true;
+            }
+        }
+    }
+
+    support_found.into_iter().all(|found| found)
+}
+
+fn device_type_to_priority(type_: vk::PhysicalDeviceType) -> i32 {
+    match type_ {
+        vk::PhysicalDeviceType::DISCRETE_GPU => 1,
+        vk::PhysicalDeviceType::INTEGRATED_GPU => 2,
+        vk::PhysicalDeviceType::VIRTUAL_GPU => 3,
+        vk::PhysicalDeviceType::CPU => 4,
+        _ => 5,
+    }
 }
